96df72da4bafdd25258b8150a9c4cc89082298ec
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-20 21:21:00 +0500

    Update update_model.py

--- update_model.py
+++ update_model.py
@@ -5,7 +5,7 @@ def update_model_from_csv(csv_path, model_name):
     try:
         df = pd.read_csv(csv_path)
         if 'value' not in df.columns:
-            raise ValueError("CSV faylida 'value' ustuni yo'q")
+            raise ValueError("CSV faylida 'value' ustuni yo‘q")
         data = df['value'].tolist()
         train_and_save_model(data, model_name)
         print(f"{model_name} modeli yangilandi.")
